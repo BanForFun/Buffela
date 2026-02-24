c19189226f98b51019d0ef03affb2d7e9fa5bdfa
John Pegioudis <thebanforfun@gmail.com>
2026-02-25 01:31:49 +0200

    Added root dir parameter to js-tools

--- js/packages/tools/js-tools.js
+++ js/packages/tools/js-tools.js
@@ -1,4 +1,5 @@
 const process = require("node:process");
+const path = require("path");
 const yargs = require('yargs')
 const { hideBin } = require("yargs/helpers");
 const { readSchemaFile, getFileOutputStream, Printer } = require('@buffela/tools-common')
@@ -7,22 +8,28 @@ const { printTypes } = require("./utils/typeUtils");
 
 yargs()
     .command({
-        command: '* <schema>',
+        command: '* <schema> [rootDir]',
         describe: 'Converts buffela schemata to JSON and generates type definitions',
         builder: (yargs) => yargs
             .positional('schema', {
                 describe: 'Schema path',
                 type: 'string'
             })
+            .positional('rootDir', {
+                describe: 'Root output directory',
+                type: 'string',
+                default: '.',
+                defaultDescription: '(Current directory)'
+            })
             .option('json', {
                 alias: 'j',
-                describe: 'Json output file or directory',
+                describe: 'Json output file or directory (relative to rootDir)',
                 type: 'string',
                 default: '.'
             })
             .option('types', {
                 alias: 't',
-                describe: 'Type output file or directory',
+                describe: 'Type output file or directory (relative to rootDir)',
                 type: 'string',
                 default: '.'
             })
@@ -40,20 +47,22 @@ yargs()
             const inputFile = readSchemaFile(argv.schema)
 
             if (argv.json) {
-                const jsonStream = getFileOutputStream(argv.json, inputFile.name + ".json")
+                const jsonPath = path.resolve(argv.rootDir, argv.json)
+                const jsonStream = getFileOutputStream(jsonPath, inputFile.name + ".json")
                 jsonStream.write(JSON.stringify(inputFile.schema, null, 2))
                 jsonStream.end()
             }
 
             if (argv.types) {
-                const typeStream = getFileOutputStream(argv.types, inputFile.name + ".d.ts")
+                const typesPath = path.resolve(argv.rootDir, argv.types)
+                const typesStream = getFileOutputStream(typesPath, inputFile.name + ".d.ts")
 
                 global.schema = parseSchema(inputFile.schema)
-                global.printer = new Printer(typeStream)
+                global.printer = new Printer(typesStream)
                 global.options = { serializerEnabled: argv.serializer, deserializerEnabled: argv.deserializer }
 
                 printTypes()
-                typeStream.end()
+                typesStream.end()
             }
         }
     })
